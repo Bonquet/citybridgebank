d1bbaf859580c80d880595fe9e16e79446f2d060
Bonquet <bonquetjet@gmail.com>
2026-02-16 00:00:26 -0600

    commit

--- public/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This file redirects any access to /public/admin/ to the proper admin dashboard.
-// The admin area does not reside under the public folder. Redirect to the
-// project-level admin index.
-
-header('Location: ../../admin/index.php');
-exit();
